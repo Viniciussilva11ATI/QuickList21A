84f6fe9997c6af431846315d4c4a5a88660697c0
Vinicius Silva <vincius_silva@eaportal.org>
2025-06-01 18:33:49 -0300

    primeira versão da função checkItem()

--- main.js
+++ main.js
@@ -24,10 +24,10 @@ function showItemsList() {
             <div class="item">
                 <div>
                     <input type="checkbox" name="list" id="item-${index}">
-                    <div class="custom-checkbox">
+                    <div class="custom-checkbox" onclick=checkItem('${item.name})>
                         <img src="./assets/checked.svg" alt="checked">
                 </div>
-                <label for="item-${index}">${item.name}</label>
+                <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
             </div>
 
             <button onclick="removeItem('${item.name}')">
@@ -54,4 +54,8 @@ function removeItem(itemName) {
     }
 
     showItemsList()
+}
+
+function checkItem() {
+
 }
